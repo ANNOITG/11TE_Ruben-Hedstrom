736b8b5caae1041184f9a52abe467156a4dd871b
ruben1337 <ruben.hedstrom@hotmail.com>
2014-06-03 10:39:45 +0200

    Programmeringsuppgifter

---
+++ 001/001/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class A
+    {
+        public void publicA()
+        {
+        }
+
+        protected void protectedA()
+        {
+        }
+
+        private void privateA()
+        {
+        }
+
+        class AB
+        {
+            public void publicAB()
+            {
+            }
+
+            protected void protectedAB()
+            {
+            }
+
+            private void privateAB()
+            {
+            }
+        }
+    }
+    class B : A
+    {
+        public void publicB()
+        {
+        }
+
+        protected void protectedB()
+        {
+        }
+
+        private void privateB()
+        {
+        }
+    }
+}
---
+++ ProductStore/ProductStore/Models/ProductStore.Models.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductStore.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal ActualCost { get; set; }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Customer { get; set; }
+
+        // Navigation property
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+
+        // Navigation properties
+        public Product Product { get; set; }
+        public Order Order { get; set; }
+    }
+}
